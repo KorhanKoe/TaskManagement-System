9318da8ce903082121ab18161544b6f828cfe3a4
Korhan <korhan.koeroglu@web.de>
2024-07-02 23:04:42 +0200

    Aktualisierungen von Kommentaren

--- src/main/java/org/taskmanagementsystem/auth/UserRepository.java
+++ src/main/java/org/taskmanagementsystem/auth/UserRepository.java
@@ -3,6 +3,6 @@ package org.taskmanagementsystem.auth;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 //Schnittstelle für den Zugriff auf Datenbankoperationen, die auf der Entität "User" basieren
-public interface UserRepository extends JpaRepository<User, Long> {     //Durch die Erweiterung werden CRUD-Operationen ermöglicht
-    User findByUsername(String username);       //sucht Benutzer anhand des Benutzernamens aus der DB
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
